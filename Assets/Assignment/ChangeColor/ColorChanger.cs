9a20e75814353ecbbc6cfad159857ca15494cf27
brianaravilla-code <brian.aravilla@ciit.edu.ph>
2026-05-24 17:42:59 +0800

    assignment

--- Assets/Assignment/ChangeColor/ColorChanger.cs
+++ Assets/Assignment/ChangeColor/ColorChanger.cs
@@ -2,16 +2,19 @@ using System.Linq.Expressions;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class ColorChanger : MonoBehaviour
 {
+    [SerializeField] private Material Base;
+    [SerializeField] private Material OFF;
+    [SerializeField] private Material ON;
+
     #region New input system
     private PlayerColor testInputs;
 
     [Header("Game Object Properties")]
-
-    [SerializeField] private Transform GameObject;
-    [SerializeField] private Vector3 ColorShift = new Vector3(0.3f, 0f, 0f);
+    [SerializeField] private Button ColorShift;
 
     [Header("Enum types")]
     [SerializeField] private EnumTypes enumTypes;
@@ -37,40 +40,13 @@ public class ColorChanger : MonoBehaviour
     {
         testInputs.ChangeColor.ColorShift.performed += ctx =>
         {
-            /*GameObject.position += ColorShift;//x,y,z //Vector2(0.3f, 0f) x,y*/
-            switch (enumTypes)
+            Base.mainTexture = OFF.mainTexture;
+                switch (enumTypes)
             {
                 case EnumTypes.OneObject:
-                    testGameObjects[0].transform.position += ColorShift;
+                    ChangeColors(, Color.red);
                     Debug.Log("call OneObject");
                     break;
-                case EnumTypes.TwoObject:
-                    testGameObjects[0].transform.position += ColorShift;
-                    testGameObjects[1].transform.position += ColorShift;
-                    Debug.Log("call TwoObject");
-                    break;
-                case EnumTypes.ThreeObject:
-                    testGameObjects[0].transform.position += ColorShift;
-                    testGameObjects[1].transform.position += ColorShift;
-                    testGameObjects[2].transform.position += ColorShift;
-                    Debug.Log("Changed 3 Objects' color");
-                    break;
-                case EnumTypes.FourObject:
-                    testGameObjects[0].transform.position += ColorShift;
-                    testGameObjects[1].transform.position += ColorShift;
-                    testGameObjects[2].transform.position += ColorShift;
-                    testGameObjects[3].transform.position += ColorShift;
-                    Debug.Log("Changed 4 Objects' color");
-                    break;
-                case EnumTypes.FiveObject:
-                    testGameObjects[0].transform.position += ColorShift;
-                    testGameObjects[1].transform.position += ColorShift;
-                    testGameObjects[2].transform.position += ColorShift;
-                    testGameObjects[3].transform.position += ColorShift;
-                    testGameObjects[4].transform.position += ColorShift;
-                    Debug.Log("Changed 5 Objects' color");
-                    break;
-
             }
         };
 
